3318e2ff4ed20da9f2dd1de74222ba93cfcf8064
Dinislam Turaykhanov <116585464+7nmw@users.noreply.github.com>
2023-02-20 14:16:25 +0300

    Add files via upload

--- LG.m
+++ LG.m
@@ -1,7 +1,7 @@
 function [outputArg1] = LG(ro,phi,w,l,p)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = (-1^p)*sqrt(2*factorial(p)/pi/w/w/factorial(p+abs(l)))*((sqrt(2)*ro/w).^abs(l));
+outputArg1 = ((-1)^p)*sqrt(2*factorial(p)/pi/w/w/factorial(p+abs(l)))*((sqrt(2)*ro/w).^abs(l));
 x=(ro/w).^2;
 outputArg1=outputArg1.*exp(1i*l*phi).*L_pl(2*x,l,p).*exp(-x);
 end
--- LG_mask.m
+++ LG_mask.m
@@ -1,44 +1,54 @@
-clc
-close all
 clear all
+x=[-500:1:500]; %a row-vector of points for the x-axis % (in pixels)
+y=[-500:1:500]; %a row-vector of points for the y-axis % (in pixels)
 
-x=[-100:0.1:100]; %a row-vector of points for the x-axis % (in pixels)
-y=[-100:0.1:100]; %a row
 [X Y]=meshgrid(x,y); % create matrices for grids of X and Y
 [phi,ro] = cart2pol(X,Y); %transform Cartesian coordinates to polar
+% w=100;
+% l=1;
 p=0;
+% res=LG(ro,phi,w,l,p);
 
 
-L11=1; %OAM of generation для накачки
+L11=4; %OAM of generation для накачки 
+L12=-4;
+% L13=1;
+% L14=3;
 
 
-w0=50; % gaussian waist beam для накачки 
+w0=200; % gaussian waist beam для накачки 
 
 V=11; %период диф. реш. в пикселях для 1
 
+
 LG11=LG(ro,phi,w0,L11,p);
-Result_1=LG11;
+LG12=LG(ro,phi,w0,L12,p);
+% LG13=LG(ro,phi,w0,L13,p);
+% LG14=LG(ro,phi,w0,L14,p);
+% Result_1=(1/3)^0.5*LG11+(1/3)^0.5*LG12+(1/3)^0.5*LG13;
+a=1;
+b=1;
+Result_1=(a)^0.5*LG11+(b)^0.5*LG12*exp(0i*pi);
+PHASE_1=((atan2(imag(Result_1), real(Result_1)))-pi*abs(Result_1));
+PHASE_1=mod(PHASE_1-2*pi*X/V,2*pi).*abs(Result_1);
+PHASE_1=PHASE_1/max(max(PHASE_1));
 
-PHASE_0=angle(Result_1);
-PHASE_1=(angle(Result_1)-pi*abs(Result_1));
-PHASE_2=mod(PHASE_1-2*pi*X/V,2*pi).*abs(Result_1);
-PHASE_3=PHASE_2/max(max(PHASE_2));
-PHASE_4=mod(PHASE_0-2*pi*X/V,2*pi);
-% mesh(PHASE_1);set(gca,'view',[0 90]);colormap gray;
 
+%Построение итоговой интенсивности
+Result_1=Result_1/max(max(Result_1));
 figure
-colormap gray
-mesh(X,Y,abs(PHASE_3));
+mesh(X,Y,abs(Result_1));
 set(gca,'view',[0 90]);
 
+%Построение итоговой маски
 figure
-colormap gray
-mesh(X,Y,abs(PHASE_4));
-set(gca,'view',[0 90]);
+mesh(X,Y,PHASE_1);
+set(gca,'view',[0 90]);colormap gray;
 
+
+%Сохранение файла на диск
 g=256;
 map=[1:1:g;1:1:g;1:1:g]./256;
 map=map';
 f=256;
-%imwrite(Result3.*f,map, 'Generate111.bmp');
-imwrite(PHASE_3.*f,map, 'Compensation.bmp');
+imwrite(PHASE_1.*f,map, 'Compensation.bmp');
